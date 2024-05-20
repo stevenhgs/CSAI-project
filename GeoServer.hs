4b095185679f800196829a8f487b3ebb641e20ba
ArsenBilyalov <132441585+ArsenBilyalov@users.noreply.github.com>
2024-05-20 18:07:48 +0200

    Update GeoServer.hs

--- GeoServer.hs
+++ GeoServer.hs
@@ -1,36 +1,21 @@
-type Radius = Float
-type Side = Float
-type Point = (Float, Float, Float)
+{-# OPTIONS_GHC -Wno-missing-fields #-}
+module GeoServer where
+import Graphics.UI.GLUT
+import Region
+import RenderRegion
+import Data.Char (GeneralCategory(NotAssigned))
 
-data Region = -- Abstract syntax tree
-    Sphere Radius
-    | Cube Side
-    | Translate Region Point
-    | Outside Region 
-    | Intersection Region Region
-    | Union Region Region
-
-data RegionAlgebra a =
-  RAlg {
-    ra_sphere  :: Radius -> a,
-    ra_cube :: Side -> a,
-    ra_translate  :: a -> Point -> a,
-    ra_outside  :: a -> a,
-    ra_intersection  :: a -> a -> a,
-    ra_union :: a -> a -> a
-  }
 
 foldRegion :: RegionAlgebra a -> Region -> a
-foldRegion alg (Sphere r)       = ra_sphere alg r
-foldRegion alg (Cube s)         = ra_cube alg s
-foldRegion alg (Translate r p)  = ra_translate alg (foldRegion alg r) p
-foldRegion alg (Outside r)      = ra_outside alg (foldRegion alg r)
-foldRegion alg (Intersection r1 r2) = ra_intersection alg (foldRegion alg r1) (foldRegion alg r2)
-foldRegion alg (Union r1 r2) = ra_union alg (foldRegion alg r1) (foldRegion alg r2)
-
+foldRegion alg (SphereGS r)       = ra_sphere alg r
+foldRegion alg (CubeGS s)         = ra_cube alg s
+foldRegion alg (TranslateGS r p)  = ra_translate alg (foldRegion alg r) p
+foldRegion alg (OutsideGS r)      = ra_outside alg (foldRegion alg r)
+foldRegion alg (IntersectionGS r1 r2) = ra_intersection alg (foldRegion alg r1) (foldRegion alg r2)
+foldRegion alg (UnionGS r1 r2) = ra_union alg (foldRegion alg r1) (foldRegion alg r2)
 
 -- inRegion interpretation
-inRegionAlgebra :: RegionAlgebra (Point -> Bool)
+inRegionAlgebra :: RegionAlgebra (PointGS -> Bool)
 inRegionAlgebra = RAlg {
   ra_sphere = \r (x, y, z) -> x^2 + y^2 + z^2 <= r^2,
   ra_cube = \s (x, y, z) -> abs x <= s/2 && abs y <= s/2 && abs z <= s/2,
@@ -40,6 +25,30 @@ inRegionAlgebra = RAlg {
   ra_union = \r1 r2 p -> r1 p || r2 p
 }
 
+-- renderRegion interpretation
+renderAlgebra :: RegionAlgebra DisplayCallback
+renderAlgebra = RAlg {
+  ra_sphere = drawSphere,
+  ra_cube = drawCube,
+  ra_translate = drawTranslate,
+  ra_union = drawUnion
+}
+
 -- serialize interpretation
 serialize :: Region -> String
 serialize = undefined
+
+
+-- display logic
+displayRegion :: Region -> IO ()
+displayRegion region = do
+  (_progName, _args) <- getArgsAndInitialize
+  _window <- createWindow "GeoServer"
+  displayCallback $= drawing region
+  mainLoop
+
+drawing :: Region -> DisplayCallback
+drawing region = do
+    clear [ ColorBuffer, DepthBuffer, StencilBuffer]
+    foldRegion renderAlgebra region
+    flush
