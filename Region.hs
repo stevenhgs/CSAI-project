050fd9ad46f09543b159f5f3a699697ce05d4862
ArsenBilyalov <132441585+ArsenBilyalov@users.noreply.github.com>
2024-05-23 18:50:13 +0200

    Update Region.hs

--- Region.hs
+++ Region.hs
@@ -2,12 +2,16 @@ module Region where
 
 type RadiusGS = Double
 type SideGS = Double
+type BaseGS = Double
+type HeightGS = Double
 type PointGS = (Double, Double, Double)
 
 data Region = -- Abstract syntax tree
     SphereGS RadiusGS
     | CubeGS SideGS
+    | ConeGS BaseGS HeightGS
     | TranslateGS Region PointGS
+    | RotateGS Region PointGS
     | OutsideGS Region 
     | IntersectionGS Region Region
     | UnionGS Region Region
@@ -16,7 +20,9 @@ data RegionAlgebra a =
   RAlg {
     ra_sphere  :: RadiusGS -> a,
     ra_cube :: SideGS -> a,
+    ra_cone :: BaseGS -> HeightGS -> a,
     ra_translate  :: a -> PointGS -> a,
+    ra_rotate :: a -> PointGS -> a,
     ra_outside  :: a -> a,
     ra_intersection  :: a -> a -> a,
     ra_union :: a -> a -> a
